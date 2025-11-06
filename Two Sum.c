529124018e3cebfb08618ef67a49329d7971c4e6
Areeba <connect.areeba@gmail.com>
2025-11-06 12:07:02 +0500

    Create Two Sum.c

---
+++ Two Sum.c
@@ -0,0 +1,16 @@
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+ 
+    *returnSize = 2; 
+
+    for (int i = 0; i < numsSize; i++) {
+        for (int j = i + 1; j < numsSize; j++) {
+            if (nums[i] + nums[j] == target) {
+                int* result = (int*)malloc(2 * sizeof(int));
+                result[0] = i;
+                result[1] = j;
+                return result;
+            }
+        }
+    }
+    return NULL; 
+}
