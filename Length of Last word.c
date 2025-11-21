ff37493040155b271a21afac246f94a2f0d5726a
Areeba <connect.areeba@gmail.com>
2025-11-21 23:39:13 +0500

    Implement function to find length of last word

---
+++ Length of Last word.c
@@ -0,0 +1,19 @@
+int lengthOfLastWord(char * s) {
+    int length = 0;
+    int i = 0;
+
+    while (s[i] != '\0') {
+        i++;
+    }
+    i--; 
+
+    while (i >= 0 && s[i] == ' ') {
+        i--;
+    }
+    while (i >= 0 && s[i] != ' ') {
+        length++;
+        i--;
+    }
+
+    return length;
+}
